303cb4399eb45f512e5ee908a5e11e71b142d7ae
Naeem <naeemark@gmail.com>
2021-04-15 04:09:59 +0800

    excludes url and token

--- test_script.py
+++ test_script.py
@@ -1,7 +1,10 @@
 from locust import HttpUser, task, between
 
-class WebsiteTestUser(HttpUser):
-    wait_time = between(0.5, 3.0)
+HOST_URL = "http://********TO BE REPLACED********/"
+AUTHORIZATION_TOKEN = "Token eyJ0eXA********TO BE REPLACED********IUzI1NiJ9"
+
+class ApiTestUser(HttpUser):
+    wait_time = between(0.1, 0.5)
     
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
@@ -11,15 +14,22 @@ class WebsiteTestUser(HttpUser):
         """ on_stop is called when the TaskSet is stopping """
         pass
 
-    @task(1)
-    def check_health(self):
-        self.client.get("http://********TO BE REPLACED********/update_data")
+    # @task(1)
+    # def check_health(self):
+    #     self.client.get(HOST_URL)
+
 
     # @task(2)
     # def login(self):
-    #     self.client.post("http://********TO BE REPLACED********/auth_service/login/", {"email":"android@gmail.com", "password":"aaaaaa"})
+    #     url = HOST_URL + "/api/v3.4/auth_service/login/"
+    #     self.client.post(url, {"email":"android@gmail.com", "password":"aaaaaa"})
 
-    # @task(3)
-    # def feed(self):        
-    #     self.client.get("http://********TO BE REPLACED********/social_service/post/?score=true&measurement=true&post_from=followings,public,group", headers={"Authorization":  "Token eyJ0eXAi.********TO BE REPLACED********YIZ4AYsdsQ5sdjLtN"}
-    #     )
+
+    @task(3)
+    def feed(self):
+        url = HOST_URL + "/api/v3.5/social_service/post/?score=true&measurement=true&post_from=followings,public,group"   
+        self.client.get(url, headers={"Authorization":  AUTHORIZATION_TOKEN})
+
+
+
+# Execution Command: `locust -f test_script.py`
